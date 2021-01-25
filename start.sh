14576c582ab86f56362471a4621cc1457d4897c0
Flyeaxxxc <77344229+Flyeaxxxc@users.noreply.github.com>
2021-01-25 12:28:28 -0500

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+mkdir documents
+touch bot.txt
+touch name.txt
+touch owner.txt
+touch token.txt
+mv bot.txt documents
+mv name.txt documents
+mv ownertxt documents
+mv token.txt documents
+python3 mods.py
